0d1288e6909d6c1110762b24555cd750ed40fb1b
Hafidh Rendyanto <hafidh.rendyanto@gmail.com>
2019-04-10 18:06:26 +0700

    Final

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,7 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /mnt/sda1/Documents/University/OOP/EngiFarm
-# Build directory: /mnt/sda1/Documents/University/OOP/EngiFarm
+# Source directory: /home/HafidhRendyanto/Documents/Programing/OOP/Engi's Farm V2
+# Build directory: /home/HafidhRendyanto/Documents/Programing/OOP/Engi's Farm V2
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(AllTest "/mnt/sda1/Documents/University/OOP/EngiFarm/bin/EngiFarmTest")
+add_test(AllTest "/home/HafidhRendyanto/Documents/Programing/OOP/Engi's Farm V2/bin/EngiFarmTest")
+set_tests_properties(AllTest PROPERTIES  _BACKTRACE_TRIPLES "/home/HafidhRendyanto/Documents/Programing/OOP/Engi's Farm V2/CMakeLists.txt;49;add_test;/home/HafidhRendyanto/Documents/Programing/OOP/Engi's Farm V2/CMakeLists.txt;0;")
